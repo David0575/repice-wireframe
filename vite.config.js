7651cce199ab37883efea917b7a81f5b04210977
David0575 <david0575@gmai.com>
2025-03-05 12:26:29 +0800

    嘗試設定讓githubpage可以看react專案

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/repice-wireframe/' // 為了設定讓github page 可以正常運作
 })
